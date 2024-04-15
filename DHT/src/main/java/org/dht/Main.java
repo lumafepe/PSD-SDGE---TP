cfc9240099ccb8083aba37a8eba58fa13b8a5c84
Rui Oliveira <rpoliveira02@gmail.com>
2024-04-15 14:56:38 +0100

    feat: hash ownership

--- DHT/src/main/java/org/dht/Main.java
+++ DHT/src/main/java/org/dht/Main.java
@@ -6,10 +6,19 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
+import java.util.List;
 
 public class Main {
     private static final Logger logger = LogManager.getLogger();
     private static final ConfigManager configManager = ConfigManager.getInstance();
+
+    private static void runServer() throws IOException, InterruptedException {
+        ServerBuilder.forPort(configManager.getPort())
+                .addService(new DHTService())
+                .build()
+                .start()
+                .awaitTermination();
+    }
 
     public static void main(String[] args) throws IOException, InterruptedException {
         logger.info("Starting DHT Node");
@@ -21,11 +30,8 @@ public class Main {
             return;
         }
 
-        ServerBuilder.forPort(configManager.getPort())
-                .addService(new DHTService())
-                .build()
-                .start()
-                .awaitTermination();
+        runServer();
+
         logger.info("Terminating DHT Node");
     }
 }
