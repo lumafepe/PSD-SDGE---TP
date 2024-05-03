37f2c438f075e01fa0fdee28ac4c016618411047
MiguelBraga2 <mgdsbraga@gmail.com>
2024-05-03 10:36:59 +0100

    Added proto and connection to central server

--- Client/src/main/java/org/example/Threads/Controller.java
+++ Client/src/main/java/org/example/Threads/Controller.java
@@ -1,5 +1,8 @@
 package org.example.Threads;
 
+import client.messages.Register;
+import client.messages.Message;
+import client.messages.Type;
 import org.example.CBcast;
 import org.example.CRDTs.GOSet;
 import org.example.CRDTs.ORset;
@@ -9,36 +12,70 @@ import org.zeromq.SocketType;
 import org.zeromq.ZContext;
 import org.zeromq.ZMQ;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.nio.ByteBuffer;
+
+import com.google.protobuf.ByteString;
+
 public class Controller extends Thread{
     GOSet fileRatingsCRDT;
     ORset filesCRDT;
     ORset usersCRDT;
     CBcast causalBroadcast;
+    Socket centralServer;
+    PrintWriter out;
+    BufferedReader in;
 
-    public Controller() {
+    public Controller() throws IOException {
         this.fileRatingsCRDT = new GOSet();
         this.filesCRDT = new ORset();
         this.usersCRDT = new ORset();
         this.causalBroadcast = new CBcast();
+        centralServer = new Socket("localhost", 4321);
+        out = new PrintWriter(centralServer.getOutputStream(), true);
+        in = new BufferedReader(new InputStreamReader(centralServer.getInputStream()));
     }
 
     // Sockets Sub, Pub
     public void run() {
         System.out.println("Controller started working");
         try (ZContext context = new ZContext();
-             ZMQ.Socket subscriber = context.createSocket(SocketType.SUB);
-             ZMQ.Socket broadcast = context.createSocket(SocketType.PUSH)) {
-            subscriber.connect("tcp://localhost:5001");
-            subscriber.connect("tcp://localhost:5003");
-            subscriber.subscribe("");
-            broadcast.connect("tcp://localhost:5002");
+             ZMQ.Socket router = context.createSocket(SocketType.ROUTER)) {
+            router.bind("tcp://localhost:5001");
 
             while (true) {
-                byte[] line = subscriber.recv();
-                String msgReceived = new String(line);
+                byte[] identity = router.recv(0);
+
+                // Discard empty delimiter frame
+                router.recv(0);
+
+                // Receive request message
+                byte[] request = router.recv(0);
+                String msgReceived = new String(request);
+                System.out.println("Received request from client " + new String(identity) + ": " + new String(request));
 
                 if (msgReceived.startsWith("/chat")) {
                     continue;
+                }
+                else if (msgReceived.startsWith("/register")){
+                    Register registerRequest = Register.newBuilder()
+                            .setUsername("miguel")
+                            .setPassword("miguel")
+                            .build();
+
+                    Message reply = send(Message.newBuilder()
+                            .setType(Type.REGISTER)
+                            .setRegister(Register.newBuilder()
+                                    .setUsername("miguel")
+                                    .setPassword("miguel")
+                                    .build())
+                            .build());
+
                 }
                 else if (msgReceived.startsWith("/addFile")) {
                     String fileName = msgReceived.substring("/addFile".length());
@@ -70,7 +107,7 @@ public class Controller extends Thread{
 
                     byte[] operationSerialized = o.serialize();
                     System.out.println("Serialized data: " + operationSerialized);
-                    broadcast.send(operationSerialized);
+                    //broadcast.send(operationSerialized);
                 }
                 else if (msgReceived.startsWith("/listRates")) {
                     if (this.fileRatingsCRDT.getRatings().isEmpty()) {
@@ -84,7 +121,23 @@ public class Controller extends Thread{
                     Operation o = Operation.deserialize(msgReceived.getBytes());
                     System.out.println("Received operation: " + o);
                 }
+
             }
         }
+    }
+
+    private Message send(Message msg) {
+        try {
+            centralServer.getOutputStream().write(msg.toByteArray());
+            byte[] buffer = new byte[1048576];
+            int read = centralServer.getInputStream().read(buffer);
+            Message response = Message.parseFrom(ByteBuffer.wrap(buffer, 0, read));
+            return response;
+        } catch(UnknownHostException e) {
+            System.exit(1);
+        } catch(IOException e) {
+            System.exit(1);
+        }
+        return null;
     }
 }
--- Client/src/main/java/org/example/Threads/Main.java
+++ Client/src/main/java/org/example/Threads/Main.java
@@ -4,22 +4,30 @@ import org.zeromq.SocketType;
 import org.zeromq.ZContext;
 import org.zeromq.ZMQ;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Controller c = new Controller();
+        Controller c = null;
+        try {
+            c = new Controller();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
         c.start();
 
         Scanner sc = new Scanner(System.in);
         try (ZContext context = new ZContext();
-             ZMQ.Socket sendCommand = context.createSocket(SocketType.PUB)) {
-
-            sendCommand.bind("tcp://localhost:5001");
+             ZMQ.Socket sendCommand = context.createSocket(SocketType.REQ)) {
+            sendCommand.connect("tcp://localhost:5001");
             while (sc.hasNextLine()) {
                 String line = sc.nextLine();
                 System.out.println("Send line to controller: " + line);
                 sendCommand.send(line.getBytes());
+
+                byte[] reply = sendCommand.recv(0);
+                System.out.println("Received reply from ROUTER: " + new String(reply));
             }
         }
     }
