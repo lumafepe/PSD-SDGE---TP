5abf7484fe4595c8acddf35910244118939ccefb
lumafepe <lumafepe@gmail.com>
2024-05-17 15:46:06 +0100

    client dockerfile

--- client/src/main/java/org/client/Main.java
+++ client/src/main/java/org/client/Main.java
@@ -2,15 +2,27 @@ package org.client;
 
 import org.cli.Root;
 import org.cli.Sender;
+import java.util.Map;
 
 
 public class Main {
     public static void main(String[] args) {
 
         Sender router = Sender.getInstance();
+        Map<String, String> env = System.getenv();
 
-        router.setIdentity(args[0]);
-        router.setAddress("172.25.0.2"); // todo: hardcoded
+        String identity,address;
+
+        if (args.length > 0) {
+            identity = args[0];
+        } else {
+            identity = env.getOrDefault("ID", "defaultID");
+        }
+
+        address = env.getOrDefault("IP", "172.25.0.2"); // todo: Not hardcode IP
+
+        router.setIdentity(identity);
+        router.setAddress(address);
         router.setPort(4321);
 
         router.setup();
--- docker-compose-generator.py
+++ docker-compose-generator.py
@@ -10,7 +10,7 @@ except:
 
 getConfig = lambda x : {
     "dht":{
-        "address": f"192.168.32.{3+x}",
+        "address": f"dhtnode{x}",
         "port": 4200,
         "baseDirectory": "/tmp/dht/",
         "tokenCount": 20,
@@ -55,7 +55,7 @@ parser = argparse.ArgumentParser(
                     epilog='generator')
 
 
-#parser.add_argument('-c', '--clients',choices=range(1, 999),type=int,help='number of clients to create')
+parser.add_argument('-c', '--clients',choices=range(0, 999),type=int,help='number of clients to create')
 parser.add_argument('-d', '--dht',choices=range(0, 999),type=int,help='number of dht nodes to create',default=1)
 
 args = parser.parse_args()
@@ -69,13 +69,24 @@ genNode = lambda x: (f'dhtnode{x}',{
                 f'./configFiles/config_{x}.yml:/config.yml',
                 f'./dhtData/dhtnode{x}:/tmp/dht/'
             ],
-            'networks': {
-                'lost':{
-                    'ipv4_address' : f"192.168.32.{3+x}"
-                }
-            },
+            'networks': [
+                'lost'
+            ],
             'depends_on': ['central_server'] + [f'dhtnode{i}' for i in range(x)]
         })
+
+genClient = lambda x: (f'client{x}',{
+    'build': {
+                'context': './client', 
+                'dockerfile': 'Dockerfile'
+            },
+    'networks': [ 'lost'],
+    'depends_on': ['central_server'],
+    'environment': [
+        f"ID={x}",
+        f"IP=central_server"
+    ]
+})
 
 
 for i in range(args.dht):
@@ -83,6 +94,10 @@ for i in range(args.dht):
     config = getConfig(i)
     with io.open(f'./configFiles/config_{i}.yml', 'w', encoding='utf8') as outfile:
         yaml.dump(config, outfile, default_flow_style=False, allow_unicode=True,indent=2)
+    data['services'][name] = d
+    
+for i in range(args.clients):
+    name,d = genClient(i)
     data['services'][name] = d
 
 # Write YAML file
